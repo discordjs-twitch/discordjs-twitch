ac5c08a7e1a0a102c7e2e68eccd01b0be474689f
bad-boy-discord <andreyzero123@gmail.com>
2022-03-11 21:17:40 +0300

    :memo: Documentation fix

--- src/classes/Manager.ts
+++ src/classes/Manager.ts
@@ -203,3 +203,58 @@ export class Manager extends TypedEmitter<ManagerEvents> {
     });
   }
 }
+
+/**
+ * Module Options
+ * @typedef {Object} Options
+ * @prop {TwitchCredentials} credentials {@link https://dev.twitch.tv/console/apps|Twitch Credentials}
+ * @prop {string} [DBName] Database Name
+ * @prop {string} [DBPath] Database Path
+ * @prop {boolean} [debug] Debug Mode
+ */
+
+/**
+ * Twitch Credentials
+ * @typedef {Object} TwitchCredentials
+ * @prop {string} clientID Twitch Client ID
+ * @prop {string} clientSecret Twitch Client Secret
+ */
+
+/**
+ * Guild Data
+ * @typedef {Object} GuildData
+ * @prop {StreamerObject[]} streamers Twitch Streamers Array
+ * @prop {string} channelID Channel ID where notifications will be posted
+ */
+
+/**
+ * Streamer Object
+ * @typedef {Object} StreamerObject
+ * @prop {string} name Streamer Username
+ * @prop {string|null} latestStream Latest Stream
+ */
+
+/**
+ * Stream Data Object
+ *
+ * @typedef {Object} StreamerData
+ * @prop {string} username Streamer Username
+ * @prop {string} profile_pic Profile Avatar
+ * @prop {string} title Stream Name
+ * @prop {string} thumbnail Stream Thumbnail
+ * @prop {number} date When stream started
+ */
+
+/**
+ * Error Object
+ * @typedef {Object} ErrorObject
+ * @prop {boolean} status Status
+ * @prop {string} message Returned Message
+ */
+
+/**
+ * Emiited when Someone started a Stream
+ *
+ * @event Manager#streamStarted
+ * @param {StreamData} data Stream Object
+ */
--- src/types.ts
+++ src/types.ts
@@ -5,50 +5,20 @@ export interface Options {
   debug?: boolean;
 }
 
-/**
- * Module Options
- * @typedef {Object} Options
- * @prop {TwitchCredentials} credentials {@link https://dev.twitch.tv/console/apps|Twitch Credentials}
- * @prop {string} [DBName] Database Name
- * @prop {string} [DBPath] Database Path
- * @prop {boolean} [debug] Debug Mode
- */
-
 export interface TwitchCredentials {
   clientID: string;
   clientSecret: string;
 }
-
-/**
- * Twitch Credentials
- * @typedef {Object} TwitchCredentials
- * @prop {string} clientID Twitch Client ID
- * @prop {string} clientSecret Twitch Client Secret
- */
 
 export interface GuildData {
   streamers: StreamerObject[];
   channelID: string;
 }
 
-/**
- * Guild Data
- * @typedef {Object} GuildData
- * @prop {StreamerObject[]} streamers Twitch Streamers Array
- * @prop {string} channelID Channel ID where notifications will be posted
- */
-
 export interface StreamerObject {
   name: string;
   latestStream: string | null;
 }
-
-/**
- * Streamer Object
- * @typedef {Object} StreamerObject
- * @prop {string} name Streamer Username
- * @prop {string|null} latestStream Latest Stream
- */
 
 export interface ManagerEvents {
   streamStarted: (data: StreamData) => void;
@@ -62,32 +32,9 @@ export interface StreamData {
   date: number;
 }
 
-/**
- * Stream Data Object
- *
- * @typedef {Object} StreamerData
- * @prop {string} username Streamer Username
- * @prop {string} profile_pic Profile Avatar
- * @prop {string} title Stream Name
- * @prop {string} thumbnail Stream Thumbnail
- * @prop {number} date When stream started
- */
-
 export interface ErrorObject {
   status: boolean;
   message: string;
 }
 
-/**
- * Error Object
- * @typedef {Object} ErrorObject
- * @prop {boolean} status Status
- * @prop {string} message Returned Message
- */
 
-/**
- * Emiited when Someone started a Stream
- *
- * @event Manager#streamStarted
- * @param {StreamData} data Stream Object
- */
